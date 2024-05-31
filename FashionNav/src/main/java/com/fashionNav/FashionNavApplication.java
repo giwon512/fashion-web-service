3b4eca228e8f337e1fba4d5ab79fb2f34904bd98
이지원 <giwon512@naver.com>
2024-05-31 18:23:04 +0900

    스프링부트 프로젝트 추가

---
+++ FashionNav/src/main/java/com/fashionNav/FashionNavApplication.java
@@ -0,0 +1,13 @@
+package com.fashionNav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionNavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionNavApplication.class, args);
+	}
+
+}
