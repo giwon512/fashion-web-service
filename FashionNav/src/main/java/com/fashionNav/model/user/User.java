d9f7c1c642ff433669890dbf5327d25614939294
LEEDAEJIN <ldj4241@naver.com>
2024-06-03 12:56:50 +0900

    User entity 생성

---
+++ FashionNav/src/main/java/com/fashionNav/model/user/User.java
@@ -0,0 +1,22 @@
+package com.fashionNav.model.user;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+    private int userId;  // u_id와 매핑
+    private String password;  // u_pw와 매핑
+    private String name;  // u_name과 매핑
+    private String email;  // u_email과 매핑
+    private LocalDateTime createdAt;  // u_regdate와 매핑
+    private LocalDateTime updatedAt;  // u_moddate와 매핑
+}
