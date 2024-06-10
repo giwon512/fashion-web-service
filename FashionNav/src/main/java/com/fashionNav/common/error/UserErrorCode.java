57d9ff5cf8491ec904e00c826d03e1dad3a8f42f
LEEDAEJIN <ldj4241@naver.com>
2024-06-10 09:59:34 +0900

    유저 에러코드 추가

--- FashionNav/src/main/java/com/fashionNav/common/error/UserErrorCode.java
+++ FashionNav/src/main/java/com/fashionNav/common/error/UserErrorCode.java
@@ -3,6 +3,7 @@ package com.fashionNav.common.error;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 
 /*
@@ -15,6 +16,10 @@ import lombok.Getter;
 public enum UserErrorCode implements ErrorCodeIfs {
 
     USER_NOT_FOUND(400,1404,"사용자를 찾을 수 없음."),
+    USER_ALREADY_EXISTS(HttpStatus.CONFLICT.value(),1405,"이미 존재하는 유저."),
+    USER_NOT_ALLOWED(HttpStatus.FORBIDDEN.value(), 1406,"허용되지 않은 유저."),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED.value(), 1407,"패스워드가 올바르지 않습니다.");
+
 
     ;
 
