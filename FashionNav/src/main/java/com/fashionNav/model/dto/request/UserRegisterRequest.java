4ca2cc4a3e3a19e861a21eba87a47cc2c65cc82b
LEEDAEJIN <ldj4241@naver.com>
2024-06-11 09:51:31 +0900

    User관련 검증 추가

--- FashionNav/src/main/java/com/fashionNav/model/dto/request/UserRegisterRequest.java
+++ FashionNav/src/main/java/com/fashionNav/model/dto/request/UserRegisterRequest.java
@@ -2,6 +2,8 @@ package com.fashionNav.model.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,12 +12,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserRegisterRequest {
-    @NotBlank
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "비밀번호는 최소 8자 이상이어야 하며, 대문자, 소문자, 숫자, 특수문자를 포함해야 합니다.")
     private String password;
 
-    @NotBlank
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
+    @Size(max = 50, message = "이름은 최대 10자까지 허용됩니다.")
     private String name;
 
-    @Email
+    @Email(message = "유효한 이메일 주소를 입력해주세요.")
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
     private String email;
 }
