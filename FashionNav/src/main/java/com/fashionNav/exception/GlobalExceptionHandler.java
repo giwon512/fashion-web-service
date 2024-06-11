9bc6e626fb3db3648b25b032bf630e73c1a62a4e
LEEDAEJIN <ldj4241@naver.com>
2024-06-11 15:38:57 +0900

    GlobalExceptionHandler 을 이용해 AccessDeniedException 예외 처리

--- FashionNav/src/main/java/com/fashionNav/exception/GlobalExceptionHandler.java
+++ FashionNav/src/main/java/com/fashionNav/exception/GlobalExceptionHandler.java
@@ -4,11 +4,13 @@ package com.fashionNav.exception;
 import com.fashionNav.common.api.Api;
 import com.fashionNav.common.api.Result;
 import com.fashionNav.common.error.ErrorCode;
+import com.fashionNav.common.error.UserErrorCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -53,5 +55,15 @@ public class GlobalExceptionHandler {
                 .status(HttpStatus.BAD_REQUEST)
                 .body(Api.Error(Result.ERROR(ErrorCode.BAD_REQUEST, "Required request body is missing.")));
     }
+
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<Api<Object>> handleAccessDeniedException(AccessDeniedException e) {
+        log.error("Access Denied: {}", e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN) // 403 Forbidden 상태 코드 사용
+                .body(Api.Error(Result.ERROR(UserErrorCode.USER_NOT_ALLOWED, "Access is denied")));
+    }
+
 
 }
