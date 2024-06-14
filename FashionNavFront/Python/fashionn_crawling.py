db49146a6a44d67f32a16dd40f2f50be9cb477eb
이지원 <giwon512@naver.com>
2024-06-14 17:57:38 +0900

    크롤링 관련 코드 제작

---
+++ FashionNavFront/Python/fashionn_crawling.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+
+def create_soup(url):
+    res = requests.get(url)
+    res.raise_for_status()
+    soup = BeautifulSoup(res.text, "lxml")
+    return soup
+
+
+def scrape_news():
+    print("[FashionN New scraping]")
+    #기본 뉴스 페이지에서 1, 2페이지의 뉴스를 긁어옴(30개)
+    for i in range(1, 3):
+        url = "https://www.fashionn.com/board/list_new.php?page={}&table=1004".format(i)
+        soup = create_soup(url)
+        news_list_container = soup.find("ul", {"class" : "list_type_list01"})
+        news_list = news_list_container.find_all("li")
+        for news in news_list:
+            img_src = "https://www.fashionn.com" + news.find("img")["src"]
+            title = news.dd.find("a")["title"]
+            link = "https://www.fashionn.com/board/" + news.dd.find("a")["href"]
+            desc = news.dd.get_text()
+            date = news.find("span", {"class":"date"}).get_text()
+            print(img_src)
+            print(title)
+            print(link)
+            print(desc)
+            print(date)
+            print("본문 내용")
+            soup = create_soup(link)
+            content = soup.find("div", {"class":"view_body"}).get_text()
+            print(content)
+            print("=================================================")
+
+
+
+
+if __name__ == "__main__":
+    scrape_news()
